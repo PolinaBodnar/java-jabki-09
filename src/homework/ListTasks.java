7338a0664c587ca15b94b7e24f1016c9913e36fc
Polina Bodnar <honey@list.ru>
2025-05-28 14:13:37 +0200

    Homework_2

--- src/homework/ListTasks.java
+++ src/homework/ListTasks.java
@@ -5,116 +5,141 @@ import java.util.stream.Collectors;
 
 public class ListTasks {
 
-    // Вывести список из 5 чисел через пробел
-    public static void printList(List<Integer> list) {
-        for (Integer num : list) {
-            System.out.print(num + " ");
+    public static void printList(List list) {
+        for (Object obj : list) {
+            System.out.print(obj + " ");
         }
         System.out.println();
     }
 
-    // Вывести третий элемент списка
-    public static Integer getThirdElement(List<Integer> list) {
+    public static Integer getThirdElement(List list) {
         if (list.size() < 3) {
             throw new IndexOutOfBoundsException("Меньше 3 элементов");
         }
-        return list.get(2);
+        return (Integer) list.get(2);
     }
 
-    // Удалить второй элемент списка и вывести оставшиеся
-    public static List<Integer> removeSecondElement(List<Integer> list) {
-        if (list.size() < 2) {
-            return list;
+    public static List removeSecondElement(List list) {
+        if (list.size() >= 2) {
+            list.remove(1);
         }
-        list.remove(1);
         return list;
     }
 
-    // Найти сумму всех чисел в списке
-    public static int sumList(List<Integer> list) {
+    public static int sumList(List list) {
         int sum = 0;
-        for (int num : list) sum += num;
+        for (Object obj : list) {
+            sum += (Integer) obj;
+        }
         return sum;
     }
 
-    // Подсчитать сколько раз встречается число 7
-    public static int countSevens(List<Integer> list) {
+    public static int countSevens(List list) {
         int count = 0;
-        for (int num : list) {
-            if (num == 7) count++;
-        }
-        return count;
-    }
-
-    // Проверить, содержится ли число 42
-    public static boolean contains42(List<Integer> list) {
-        return list.contains(42);
-    }
-
-    // Найти минимум и максимум
-    public static int[] minMax(List<Integer> list) {
-        if (list.isEmpty()) throw new NoSuchElementException("Пустой список");
-        int min = Collections.min(list);
-        int max = Collections.max(list);
-        return new int[]{min, max};
-    }
-
-    // Удалить все четные числа
-    public static List<Integer> removeEven(List<Integer> list) {
-        return list.stream().filter(n -> n % 2 != 0).collect(Collectors.toList());
-    }
-
-    // Создать список из массива
-    public static List<Integer> fromArray(int[] arr) {
-        List<Integer> list = new ArrayList<>();
-        for (int n : arr) list.add(n);
-        return list;
-    }
-
-    // Количество элементов > N
-    public static int countGreaterThan(List<Integer> list, int n) {
-        int count = 0;
-        for (int item : list) {
-            if (item > n) {
+        for (Object obj : list) {
+            if (((Integer) obj) == 7) {
                 count++;
             }
         }
         return count;
     }
 
-    // Создать список строк и вывести строки длиннее 5
-    public static List<String> filterLongStrings(List<String> list) {
-        return list.stream().filter(s -> s.length() > 5).collect(Collectors.toList());
+    public static boolean contains42(List list) {
+        return list.contains(42);
     }
 
-    // Инвертировать список
-    public static <T> List<T> invertList(List<T> list) {
-        List<T> inverted = new ArrayList<>(list);
-        Collections.reverse(inverted);
+    public static int[] minMax(List list) {
+        if (list.isEmpty()) {
+            throw new NoSuchElementException("Пустой список");
+        }
+        int min = (Integer) list.get(0);
+        int max = (Integer) list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            int val = (Integer) list.get(i);
+            if (val < min) {
+                min = val;
+            } else if (val > max) {
+                max = val;
+            }
+        }
+        return new int[]{min, max};
+    }
+
+    public static List removeEven(List list) {
+        List result = new ArrayList();
+        for (Object obj : list) {
+            int val = (Integer) obj;
+            if (val % 2 != 0) {
+                result.add(val);
+            }
+        }
+        return result;
+    }
+
+    public static List fromArray(int[] arr) {
+        List list = new ArrayList();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+        return list;
+    }
+
+    public static int countGreaterThan(List list, int n) {
+        int count = 0;
+        for (Object obj : list) {
+            int val = (Integer) obj;
+            if (val > n) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static List filterLongStrings(List list) {
+        List result = new ArrayList();
+        for (Object obj : list) {
+            String str = (String) obj;
+            if (str.length() > 5) {
+                result.add(str);
+            }
+        }
+        return result;
+    }
+
+    public static List invertList(List list) {
+        List inverted = new ArrayList();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            inverted.add(list.get(i));
+        }
         return inverted;
     }
 
-    // Объединить два списка без дубликатов
-    public static <T> List<T> unionNoDuplicates(List<T> list1, List<T> list2) {
-        Set<T> set = new LinkedHashSet<>();
+    public static List unionNoDuplicates(List list1, List list2) {
+        Set set = new LinkedHashSet();
         set.addAll(list1);
         set.addAll(list2);
-        return new ArrayList<>(set);
+        return new ArrayList(set);
     }
 
-    // Сумма только нечётных чисел
-    public static int sumOdd(List<Integer> list) {
+    public static int sumOdd(List list) {
         int sum = 0;
-        for (int n : list) {
-            if (n % 2 != 0) sum += n;
+        for (Object obj : list) {
+            int val = (Integer) obj;
+            if (val % 2 != 0) {
+                sum += val;
+            }
         }
         return sum;
     }
 
-    // Фильтр по первой букве "А"
-    public static List<String> filterByFirstLetterA(List<String> list) {
-        return list.stream()
-                .filter(s -> s.startsWith("А"))
-                .collect(Collectors.toList());
+    public static List filterByFirstLetterA(List list) {
+        List result = new ArrayList();
+        for (Object obj : list) {
+            String str = (String) obj;
+            if (str.startsWith("А")) {
+                result.add(str);
+            }
+        }
+        return result;
     }
 }
